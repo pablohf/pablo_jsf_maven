a0dc9d1225ece467ff139f385f1384d256ac8390
Pablo <Pablo@Pablo-PC>
2014-11-04 22:39:17 -0200

    Adição da entidade Pessoa

--- pablo_jsf_maven/src/main/java/com/br/pablo/util/FacesContextUtil.java
+++ pablo_jsf_maven/src/main/java/com/br/pablo/util/FacesContextUtil.java
@@ -4,6 +4,7 @@ import javax.faces.context.FacesContext;
 import org.hibernate.Session;
 
 public class FacesContextUtil {
+    //Seta sessão no hibernate
 
     private static final String HIBERNATE__SESSION = "hibernate_session";
 
@@ -11,7 +12,7 @@ public class FacesContextUtil {
         FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put(HIBERNATE__SESSION, session);
     }
 
-    public static Session getHIBERNATE__SESSION() {
+    public static Session getRequestSession() {
         return (Session) FacesContext.getCurrentInstance().getExternalContext().getRequestMap().get(HIBERNATE__SESSION);
     }
 }
