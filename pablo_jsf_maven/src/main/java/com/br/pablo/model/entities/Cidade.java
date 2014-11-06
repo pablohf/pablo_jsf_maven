6cf55a9cc8250d6ae9883aa2d91fb2033fb859a2
Pablo <Pablo@Pablo-PC>
2014-11-06 01:33:09 -0200

    Adição das entidades Cidade, Endereco, Estado, TipoEndereco, TipoLogradouro

---
+++ pablo_jsf_maven/src/main/java/com/br/pablo/model/entities/Cidade.java
@@ -0,0 +1,81 @@
+package com.br.pablo.model.entities;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import org.hibernate.annotations.ForeignKey;
+
+@Entity
+@Table(name = "cidade")
+public class Cidade implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    @Column(name = "IdCidade", nullable = false)
+    private Integer idCidade;
+
+    @Column(name = "Nome", length = 80, nullable = false)
+    private String nome;
+
+    @OneToMany
+    @ForeignKey(name = "CidadeEndereco")
+    private List<Endereco> enderecos;
+
+    public Cidade() {
+    }
+
+    public Integer getIdCidade() {
+        return idCidade;
+    }
+
+    public void setIdCidade(Integer idCidade) {
+        this.idCidade = idCidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(List<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 37 * hash + Objects.hashCode(this.idCidade);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cidade other = (Cidade) obj;
+        if (!Objects.equals(this.idCidade, other.idCidade)) {
+            return false;
+        }
+        return true;
+    }
+
+}
---
+++ pablo_jsf_maven/src/main/java/com/br/pablo/model/entities/TipoLogradouro.java
@@ -0,0 +1,80 @@
+package com.br.pablo.model.entities;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import org.hibernate.annotations.ForeignKey;
+
+@Entity
+@Table(name = "tipologradouro")
+public class TipoLogradouro implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    @Column(name = "IdTipoLogradouro", nullable = false)
+    private Integer idTipoLogradouro;
+    @Column(name = "DescricaoTipoLogradouro", nullable = false, length = 40)
+    private String descricaoTipoLogradouro;
+
+    @OneToMany(mappedBy = "tipologradouro", fetch = FetchType.LAZY)
+    @ForeignKey(name = "Endereco_TipoLogradouro")
+    private List<Endereco> enderecos;
+
+    public TipoLogradouro() {
+    }
+
+    public Integer getIdTipoLogradouro() {
+        return idTipoLogradouro;
+    }
+
+    public void setIdTipoLogradouro(Integer idTipoLogradouro) {
+        this.idTipoLogradouro = idTipoLogradouro;
+    }
+
+    public String getDescricaoTipoLogradouro() {
+        return descricaoTipoLogradouro;
+    }
+
+    public void setDescricaoTipoLogradouro(String descricaoTipoLogradouro) {
+        this.descricaoTipoLogradouro = descricaoTipoLogradouro;
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(List<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.idTipoLogradouro);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final TipoLogradouro other = (TipoLogradouro) obj;
+        if (!Objects.equals(this.idTipoLogradouro, other.idTipoLogradouro)) {
+            return false;
+        }
+        return true;
+    }
+}
