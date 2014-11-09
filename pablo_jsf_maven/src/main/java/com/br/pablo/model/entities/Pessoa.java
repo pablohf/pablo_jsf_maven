72a17a4ae16c77faa0bf56b9be7fe63ecb1ed916
Pablo <Pablo@Pablo-PC>
2014-11-08 22:11:29 -0200

    Finalização das Entidades

--- pablo_jsf_maven/src/main/java/com/br/pablo/model/entities/Pessoa.java
+++ pablo_jsf_maven/src/main/java/com/br/pablo/model/entities/Pessoa.java
@@ -5,9 +5,12 @@ import java.util.Date;
 import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import org.hibernate.annotations.ForeignKey;
@@ -42,10 +45,15 @@ public class Pessoa implements Serializable {
     @Column(name = "DataDeCadastro", nullable = false)
     @Temporal(javax.persistence.TemporalType.DATE)
     private Date dataDeCadastro;
+    
+    @OneToOne(mappedBy = "pessoa", fetch = FetchType.LAZY)
+    @ForeignKey(name = "EnderecoPessoa")
+    private Endereco endereco;   
 
     @ManyToOne(optional = false)
     @ForeignKey(name = "PessoSexo")
-    private Pessoa pessoa;
+    @JoinColumn(name = "IdSexo", referencedColumnName = "IdSexo")
+    private Sexo sexo;
 
     public Pessoa() {
     }
@@ -106,13 +114,29 @@ public class Pessoa implements Serializable {
         this.dataDeCadastro = dataDeCadastro;
     }
 
+    public Sexo getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Sexo sexo) {
+        this.sexo = sexo;
+    }
+    
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }    
+
     @Override
     public int hashCode() {
         int hash = 5;
         hash = 97 * hash + Objects.hashCode(this.idPessoa);
         return hash;
-    }
-
+    }   
+    
     @Override
     public boolean equals(Object obj) {
         if (obj == null) {
